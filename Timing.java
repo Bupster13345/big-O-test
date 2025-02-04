dcd0062a98169e0b5d0c0ed179c9c8d0fac8b497
caleb carter <100060470@csisd.org>
2025-02-04 14:55:10 -0600

    eh

--- Timing.java
+++ Timing.java
@@ -10,5 +10,11 @@ public class Timing {
     for (int i = 0; i < SIZE; i++) {
       arr[i] = rand.nextInt();
     }
+    
+    long start = System.currentTimeMillis();
+    Util.bubbleSort(arr);
+    long end = System.currentTimeMillis();
+    long diff = end - start; 
+    System.out.print("Start: " + start + ", End: " + end + ", Difference: " + diff);
   }
 }
