ea97ed45e4ab3a0c60b98d015f6171f4eb7061b6
caleb carter <100060470@csisd.org>
2025-02-05 14:52:08 -0600

    pretty close

--- Timing.java
+++ Timing.java
@@ -1,20 +1,36 @@
-import java.util.Random;
+import java.util.*;
+import java.io.*;
 
 public class Timing {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException{
     Random rand = new Random();
-    int SIZE = 1024;
-    int[] arr = new int[SIZE];
-
-    // generate random numbers for array
-    for (int i = 0; i < SIZE; i++) {
-      arr[i] = rand.nextInt();
+    int SIZE = 0;
+    Scanner in = new Scanner(System.in);
+    System.out.print("filename: ");
+    String fileN = in.nextLine();
+    Scanner fin = new Scanner(new File(fileN));
+    int i = 0;
+    while(fin.hasNextInt()){
+      i++;
+      fin.next();
     }
-    
-    long start = System.currentTimeMillis();
-    Util.bubbleSort(arr);
-    long end = System.currentTimeMillis();
-    long diff = end - start; 
-    System.out.print("Start: " + start + ", End: " + end + ", Difference: " + diff);
+    fin = new Scanner(new File(fileN));
+    int[] arr = new int[i];
+    for(int o = 0; o < i; o++){
+      arr[o] = fin.nextInt();
+      arr = new int[arr[o]];
+      for (int j = 0; j < arr[o]; j++){
+        arr[j] = rand.nextInt();
+      }
+     
+    for (int a = 0; a < 10; a++){
+      long start = System.currentTimeMillis();
+      Util.insertionSort(arr);
+      long end = System.currentTimeMillis();
+      long diff = end - start; 
+      System.out.println(diff);
+    }
+    System.out.println("end");
+    } 
   }
 }
